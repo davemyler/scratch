d73e09ec3ec464c487bc2271f574e371bc537c20
Dave Myler <dave.myler@maritzcx.com>
2018-10-11 15:23:02 +0100

    Add utilities lib

---
+++ Utilities/ApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Utilities
+{
+    public static class ApplicationExtensions
+    {
+        /// <summary>
+        /// Determines the local file system folder from which an application is running.
+        /// </summary>
+        /// <returns></returns>
+        public static string ExecutableDirectoryPath()
+        {
+            var codeBase = new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            var localCodeBase = codeBase.LocalPath;
+            var basePath = Path.GetDirectoryName(localCodeBase);
+            return basePath;
+        }
+    }
+}
